d4ebcf1327d76fdfe86fea88eb84b26811eb33e2
Trayan Iliev <ilievt@gmail.com>
2017-12-04 16:44:59 +0200

    Flattening the project structure - now demos are available directly at top level

--- hello-webflux/webflux-users/src/main/java/org/iproduct/demos/spring/hellowebflux/exceptions/UserDataException.java
+++
@@ -1,7 +0,0 @@
-package org.iproduct.demos.spring.hellowebflux.exceptions;
-
-public class UserDataException extends RuntimeException {
-    public UserDataException(String message) {
-        super(message);
-    }
-}
